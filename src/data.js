9cbfb4a56d6ea486bcc2707c7825abc5e44a4616
Abygail27 <abygaailrrocha@gmail.com>
2019-02-10 23:47:13 -0600

    cambio prueba

--- src/data.js
+++ src/data.js
@@ -1,16 +1,56 @@
-<<<<<<< HEAD
-=======
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-
-<<<<<<< HEAD
+const dataBase =  WORLDBANK;
+ 
+ function filterCountry(select)
+  { 
+    let sendIndicator;
+    let indicatorFilter;
+    let IndicatorsName;
+     if (select === "MEX")
+     {
+       const country =  dataBase.MEX.indicators;
+       const indicatorsName = country.filter(element=>{ 
+      return element.indicatorName === "mujeres (% de mujeres en edad de asistir al ciclo inferior de la escuela secundaria)";
+      })
   
-  //dataGlobal
-  window.worldBank 
-  
-=======
+    indicatorFilter=IndicatorsName;
+    } 
+     else if (select==="PER")
+     {
+      const country =  dataBase.PER.indicators;
+      const IndicatorsName = country.filter(element=>{ 
+      return element.indicatorName === "mujeres (% de mujeres en edad de asistir al ciclo inferior de la escuela secundaria)";
+     })
+     indicatorFilter=IndicatorsName;
+    } 
+     else if (select==="BRA")
+    {
+        const country =  dataBase.PER.indicators;
+        const IndicatorsName = country.filter(element=>{ 
+        return element.indicatorName === "mujeres (% de mujeres en edad de asistir al ciclo inferior de la escuela secundaria)";
+       })
+       indicatorFilter=IndicatorsName;
+    } 
+
+    else if (select==="CHL")
+    {
+        const country =  dataBase.PER.indicators;
+        const IndicatorsName = country.filter(element=>{ 
+        return element.indicatorName === "mujeres (% de mujeres en edad de asistir al ciclo inferior de la escuela secundaria)";
+       })
+       indicatorFilter=IndicatorsName;
+    }
+
+   
+    indicatorFilter.forEach(element => { 
+      let indicatorsData = element.data;
+      console.log(indicatorsData); 
+      sendIndicator= indicatorsData; 
+    })   
+  return sendIndicator;
+}
+window.filterCountry = filterCountry;
 
 
->>>>>>> 404064f4229161b093184efee159f8509c56bf3f
->>>>>>> d0331a29b931afdd0fc76fc691030cf405d806b8
+
+
+
