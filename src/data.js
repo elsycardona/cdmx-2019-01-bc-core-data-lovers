98fdb798e7940b94c45720ef3cece0be14515be4
Viviana Navarro <nitoname@gmail.com>
2019-02-14 11:56:44 -0600

    Test completado con 7 pruebas

--- src/data.js
+++ src/data.js
@@ -1,14 +1,11 @@
 //Funciones puras
 
 
-window.dataBank= {
+window.dataBank = {
   filter: (dataBank, countryfilter) => {
-   
-    
     let enclick = countryfilter.value;
     let dataMostrar = dataBank[enclick]
     let indicators = dataMostrar.indicators;
-    
 
     indicators.forEach(indicator => {
 
@@ -18,6 +15,16 @@ window.dataBank= {
         return countryfilter
       }
     })
+  },
 
-  }
+ filterEdu: (countryfilter,indicators) => {
+    indicators.forEach(indicator => {
+
+        let indicatorName = indicator.indicatorName;
+        let vare = new RegExp(/educación/i)
+        if (indicatorName.match(vare) != null) {
+          return countryfilter
+        }
+      })
+ }
 }
