5fb7b6425335154550918e8ee4ed0f7ed5433008
elsycardona <elsycp@gmail.com>
2019-02-25 23:44:02 -0600

    cambio de tema, falla en css

--- src/main.js
+++ src/main.js
@@ -1,118 +1,72 @@
-const botonInfo = document.getElementById('informate'); //boton para entrar a la pagína donde se generar la filtración y se mostra la data 
 const welcomePage = document.getElementById('welcomePage');
-const pageForData = document.getElementById('info');
-const dataBank = WORLDBANK;
-const pagesjumps = document.getElementById('commits', 'impact') //constantes para brincar a la section que se utiliza como una pagina
-const selectOption = document.getElementById('country-filter');
-let content = document.getElementById('show-content');
-const indicators = document.getElementById('indicator');
+const dataBank = window.WORLDBANK;
+const countryfilter = document.getElementById('countryfilter');
 const indicadoresHTML = document.getElementById('indicadores')
 
 document.getElementById('informate').addEventListener('click', () => {
   welcomePage.style.display = 'none';
-  pageForData.style.display = 'block';
-
-  jumpto(anchor)(); {
-    window.location.href = "#" + anchor;
-  }
 });
-const filterCountryIndicator = () => {
-  let select = selectOption.value; //value "MEX", "PER","BRA","CHL"
-  //console.log(select);
-  const country = window.filterCountry(select);
-  objectKey(country);
-}
 
-
+//esta función es filter en data.js
 const filtroDeIndicadores = document.getElementById('countryfilter').addEventListener('change', () => { //Impresión Database para cada país
   const enclick = countryfilter.value;
-  const dataMostrar = dataBank[enclick]
+  const dataMostrar = dataBank[enclick];
   const indicators = dataMostrar.indicators;
+
+  //función para los indicadores,buscador de EDUCACIÓN También está en data.js
   indicators.forEach(indicator => {
     const indicatorName = indicator.indicatorName;
-    const vare = new RegExp(/educación/i)
-    if(indicatorName.match(vare) != null){
-      indicadoresHTML.insertAdjacentHTML("beforeend", `<option value="${indicatorName}">${indicatorName}</option>`) 
-  }
-})
-indicadoresHTML.addEventListener('change', () => {
-let valoresExtraidosDeIndicadores = event.target.value
-indicators.forEach(element=> {
-if(valoresExtraidosDeIndicadores === element.indicatorName){
-  //console.log(element.data)
-  const valoresFinales = (element.data);
-  //console.log(valoresFinales)
-  document.getElementById('intentoDeTabla').innerHTML = '';
-  for (ejeXyears in valoresFinales){
-    if(valoresFinales.hasOwnProperty(ejeXyears)){
+    const vare = new RegExp(/laboral/i)
+    if (indicatorName.match(vare) != null) {
+      indicadoresHTML.insertAdjacentHTML("beforeend", `<option value="${indicatorName}">${indicatorName}</option>`)
+    };
+  });
+  indicadoresHTML.addEventListener('change', () => {
+    let valoresExtraidosDeIndicadores = event.target.value
+    indicators.forEach(element => {
+      if (valoresExtraidosDeIndicadores === element.indicatorName) {
+        //console.log(element.data)
+        const valoresFinales = (element.data);
+        //console.log(valoresFinales)
+        document.getElementById('intentoDeTabla').innerHTML = '';
+        for (ejeXyears in valoresFinales) {
+          if (valoresFinales.hasOwnProperty(ejeXyears)) {
 
-      let ejeYporcent = valoresFinales[ejeXyears];
-    ejeYporcent = parseFloat(ejeYporcent)
-    //console.log(parseInt(quitandoCero))
-    if (ejeYporcent > 0) {
-      //INTENTO DE TABLA//
-const fila="<tr><td>"+ejeXyears+"</td><td>"+ejeYporcent+"</td></tr>";
-const btn = document.createElement("TR");
-btn.innerHTML = fila;
-document.getElementById('intentoDeTabla').appendChild(btn);
+            let ejeYporcent = valoresFinales[ejeXyears];
+            ejeYporcent = parseFloat(ejeYporcent)
+            if (ejeYporcent > 0) {
 
-
-    //console.log(ejeXyears, ejeYporcent)
-    }
-
-      //console.log("FUNCIONA"+ ejeXyears + " y los valores " +valoresFinales[ejeXyears]);
-      
-      //HASTA AQUÍ FUNCIONA!!!
-      //INTENTO DE GRÁFICA//
-const intentoDeGrafica = document.getElementById('intentoDeGrafica');
-new Morris.Line({
- 
-  element: 'intentoDeGrafica',
-  // Registros de datos de gráficos: cada entrada en esta matriz corresponde a un punto en
-  // el gráfico
-  data: [
-        { ejeXyears: '', ejeYporcent: '' }
-  ],
-  //  El nombre del atributo de registro de datos que contiene valores de x.
-  xkey: 'ejeXyears',
-  // Una lista de nombres de atributos de registro de datos que contienen valores y.
-  ykeys: ['value'],
-  // Etiquetas para los ykeys - se mostrarán cuando pase el cursor sobre el
-  // gráfico.
-  labels: ['Value'],
-  resize: true
+              const fila = "<tr><td>" + ejeXyears + "</td><td>" + ejeYporcent.toFixed(2) + "%" + "</td></tr>";
+              const btn = document.createElement("TR");
+              btn.innerHTML = fila;
+              document.getElementById('intentoDeTabla').appendChild(btn);
+            };
+          };
+        };
+      };
+    });
+  });
+  return indicators
 });
 
+const botonGrafica = document.getElementById('visualizarGrafica').addEventListener('click', () => {
+  google.charts.load('current', { 'packages': ['corechart'] });
+  google.charts.setOnLoadCallback(drawChart);
 
-    }
-  }
-}
-})
-})
-return indicators
-})
-
-
-/*INTENTO DE GRÁFICA//
-const intentoDeGrafica = document.getElementById('intentoDeGrafica');
-new Morris.Line({
- 
-  element: 'intentoDeGrafica',
-  // Registros de datos de gráficos: cada entrada en esta matriz corresponde a un punto en
-  // el gráfico
-  data: [
-    { year: '2008', value: 20 },
-    { year: '2009', value: 10 },
-    { year: '2010', value: 5 },
-    { year: '2011', value: 5 },
-    { year: '2012', value: 20 }
-  ],
-  //  El nombre del atributo de registro de datos que contiene valores de x.
-  xkey: 'year',
-  // Una lista de nombres de atributos de registro de datos que contienen valores y.
-  ykeys: ['value'],
-  // Etiquetas para los ykeys - se mostrarán cuando pase el cursor sobre el
-  // gráfico.
-  labels: ['Value'],
-  resize: true
-});*/
+  function drawChart() {
+    var datosData = google.visualization.arrayToDataTable([
+      ['Años', 'Porcentaje'],
+      //[ejeXyears, ejeYporcent]
+      ['1970', 11],
+      ['1980', 2],
+      ['1990', 2],
+      ['2000', 2],
+      ['2010', 7]
+    ]);
+    var options = {
+      title: 'Gráfica [años vs. %]'
+    };
+    var chart = new google.visualization.PieChart(document.getElementById('intentoDeGrafica'));
+    chart.draw(datosData, options);
+  };
+});
